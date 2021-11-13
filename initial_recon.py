bfa15e5b1bd36d000a3e21b129f2c8fd870a33b6
Guy Cooper <guycooper@danstaar.com>
2021-11-13 14:45:56 -0800

    Fixed piping to see if any good ports are open.

--- initial_recon.py
+++ initial_recon.py
@@ -4,8 +4,6 @@ import sys
 import os
 import subprocess
 
-test
-
 
 # to clear screen
 def clear():
@@ -37,21 +35,27 @@ def main():
     use_enum = "enum4linux -s " +target_ip
 
     os.system("gnome-terminal -e 'bash -c \"nmap -p- --min-rate=1000 -T4 -sV -sC -v " + target_ip +"; bash\"'"); os.system("gnome-terminal -e 'bash -c \"msfdb-blackarch run; bash\"'"); os.system("gnome-terminal -e 'bash -c \"nikto -D on -h " + target_ip+"; bash\"'"); os.system("gnome-terminal -e 'bash -c \"enum4linux -a " + target_ip+"; bash\"'")
-     #nmap_terminal = "gnome-terminal -- "+ use_nmap +""
+
     # go into preferences on terminal and find under "Command", "When command exits" and set to "hold the terminal open"
     #subprocess.Popen(nmap_terminal, shell = True)
-    # print(nmap_output)
-    nmap_output = subprocess.getoutput(use_nmap); nikto_out = subprocess.getoutput(use_nikto); enum_out = subprocess.getoutput(use_enum)
 
+    # nmap_output = subprocess.getoutput(use_nmap); nikto_out = subprocess.getoutput(use_nikto); enum_out = subprocess.getoutput(use_enum)
+    # nmap_output = subprocess.run(['nmap', '-p-', '--min-rate=1000', '-T4', '-sV', '-sC', '-v'+ target_ip], stdout=subprocess.PIPE, text=True)
+    nmap_output = subprocess.run(use_nmap, stdout=subprocess.PIPE, text=True, shell=True)
+
+    #print(nmap_output.stdout)
 
     # CHECK FOR PORTS AND EXECUTE MORE RECON
     ssh_port = "22"
 
-    if ssh_port in nmap_output:
+    if ssh_port in nmap_output.stdout:
         clear()
-        print("This has port 22 open")
+        print (nmap_output.stdout)
+        print("Port 22 open, starting some vulnerability testing.\n")
+        print("Please Wait...")
     else:
         clear()
-        print("NOT HERE")
+        print (nmap_output.stdout)
+        print("NO GOOD PORTS TO TEST")
 
 main()
